a33f8e0d1579f293ad0567d2ae1d5dceb6b5b7d6
letrongtri99 <trichuot99@gmail.com>
2019-07-07 10:00:24 +0700

    sender

--- quyetde/public/js/main.js
+++ quyetde/public/js/main.js
@@ -4,6 +4,11 @@ const dislay = document.getElementById("display");
 text.onkeyup = () => {
     dislay.innerHTML = 200-text.value.length+"/200 kí tự còn lại";
 }
+text.onkeypress = () =>{
+    if(text.value.length>199){
+        return false;
+    }
+}
 button.onclick = () => {
     if(text.value.length === 0){
         alert(`Chưa nhập thông tin`);
